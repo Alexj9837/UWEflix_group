d5e854f78a8309662fd59553366c8ef4945707df
Alexj9837 <52531949+Alexj9837@users.noreply.github.com>
2023-02-23 15:50:41 +0000

    updating templates

--- UWEflix/views.py
+++ UWEflix/views.py
@@ -6,4 +6,4 @@ from django.views.generic import ListView
 
 # Create your views here.
 def home(request):
-    return render(request, "UWEflix/home.html")
+    return render(request, "UWEflix/customer/home.html")
