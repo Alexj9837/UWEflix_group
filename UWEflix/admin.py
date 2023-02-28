f65dcc407fcf18bbff9afd723dd2183c7755530a
noura2111alali <33318859+noura2111alali@users.noreply.github.com>
2023-02-28 20:41:24 +0000

    "Club representative work"

--- UWEflix/admin.py
+++ UWEflix/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Club, ClubRepresentative, Film, Showing, Ticket
 # Register your models here.
+
+
+admin.site.register(Club)
+admin.site.register(ClubRepresentative)
+admin.site.register(Film)
+admin.site.register(Showing)
+admin.site.register(Ticket)
--- UWEflix/forms.py
+++ UWEflix/forms.py
@@ -55,3 +55,47 @@ class showForm(forms.ModelForm):
             "date",
             "time",
         ]
+
+class ClubRegistrationForm(forms.Form):
+    name = forms.CharField(label='Club name', max_length=255)
+    address = forms.CharField(label='Address', max_length=255,
+        widget=forms.TextInput(attrs={'placeholder': 'Street, City, Postcode'})
+    )
+    contact_details = forms.CharField(label='Contact details', max_length=255)
+    first_name = forms.CharField(label='First name', max_length=255)
+    last_name = forms.CharField(label='Last name', max_length=255)
+    date_of_birth = forms.DateField(label='Date of birth', widget=forms.SelectDateWidget(years=range(1900, 2020)))
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        address = cleaned_data.get('address')
+        contact_details = cleaned_data.get('contact_details')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        date_of_birth = cleaned_data.get('date_of_birth')
+        if not name and not address and not contact_details and not first_name and not last_name and not date_of_birth:
+            raise forms.ValidationError('You have to write something!')
+
+class TicketPurchaseForm(forms.Form):
+    showing = forms.ModelChoiceField(queryset=Showing.objects.all())
+    payment_details = forms.CharField(label='Payment details', max_length=255)
+    def clean(self):
+        cleaned_data = super().clean()
+        showing = cleaned_data.get('showing')
+        payment_details = cleaned_data.get('payment_details')
+        if not showing and not payment_details:
+            raise forms.ValidationError('You have to write something!')
+
+
+class ClubAccountForm(forms.ModelForm):
+    class Meta:
+        model = Club
+        fields = ['name', 'address', 'contact_details']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        address = cleaned_data.get('address')
+        contact_details = cleaned_data.get('contact_details')
+        if not name and not address and not contact_details:
+            raise forms.ValidationError('You have to write something!')
