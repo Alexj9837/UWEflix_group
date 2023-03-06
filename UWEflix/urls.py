47145c75f95f3c5716e2a8c6879eda5081810854
Alexj9837 <52531949+Alexj9837@users.noreply.github.com>
2023-03-06 19:37:11 +0000

    adding more CRUD

--- UWEflix/urls.py
+++ UWEflix/urls.py
@@ -4,5 +4,7 @@ from UWEflix import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("create_club",views.create_club,name="create_club")
+    path("create_club",views.create_club,name="create_club"),
+    path("update_club",views.update_club,name="update_club"),
+    path("delete_club",views.delete_club,name="delete_club"),
 ]
--- UWEflix/views.py
+++ UWEflix/views.py
@@ -22,5 +22,25 @@ def create_club(request):
             club.save()
             return redirect("home")
     else:
-        return render(request, "UWEflix/cinema_manager/create_club.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html","form": form} )
+        return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html","form": form} )
 
+
+def update_club(request, pk):
+    club = ClubForm.objects.get(pk=pk)
+    form = ClubForm(request.POST or None, instance=club)
+
+    if request.method == "POST":
+        if form.is_valid():
+            club = form.save(commit=False)
+
+            club.save()
+            return redirect("View_clubs")
+    else:
+        return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html"})
+
+
+def delete_club(request, pk):
+    club = ClubForm.objects.get(pk=pk)
+
+    club.delete()
+    return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/headers/header_customer.html"})
