b582a388106eb6445fb80cc6acf93d781c896c84
Alexj9837 <52531949+Alexj9837@users.noreply.github.com>
2023-03-06 19:56:05 +0000

    changed template for scalablity

--- UWEflix/views.py
+++ UWEflix/views.py
@@ -10,7 +10,7 @@ from UWEflix.forms import ClubForm
 # Create your views here.
 
 def home(request):
-    return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/headers/header_customer.html"})
+    return render(request, "UWEflix/base/base.html",{"footer_content":"UWEflix/base/footer_base.html","header_content":"UWEflix/base/header_base.html"})
 
 
 def create_club(request):
@@ -22,7 +22,7 @@ def create_club(request):
             club.save()
             return redirect("home")
     else:
-        return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html","form": form} )
+        return render(request, "UWEflix/base/base.html",{"footer_content":"UWEflix/base/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html","form": form} )
 
 
 def update_club(request, pk):
@@ -36,11 +36,11 @@ def update_club(request, pk):
             club.save()
             return redirect("View_clubs")
     else:
-        return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html"})
+        return render(request, "UWEflix/base/base.html",{"footer_content":"UWEflix/base/footer_base.html","header_content":"UWEflix/cinema_manager/header_cinema_manager.html"})
 
 
 def delete_club(request, pk):
     club = ClubForm.objects.get(pk=pk)
 
     club.delete()
-    return render(request, "UWEflix/customer/home.html",{"footer_base":"UWEflix/footers/footer_base.html","header_content":"UWEflix/headers/header_customer.html"})
+    return render(request, "UWEflix/customer/home.html",{"footer_content":"UWEflix/footers/footer_base.html","header_content":"header_cinema_manager.html"})
