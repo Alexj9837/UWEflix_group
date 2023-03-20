a99233d6946725ec9b8751ff984765bd77f4b016
noura2111alali <33318859+noura2111alali@users.noreply.github.com>
2023-03-20 12:54:39 +0000

    updated and merged changes

--- UWEflix/urls.py
+++ UWEflix/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("create_club", views.create_club,name="create_club"),
-    path("shows", views.get_shows, name="get_shows"),
-    path("purchase_ticket", views.purchase_ticket, name="purchase_ticket"),
-    path("book_show", views.book_show, name="book_show"),
-    path("manage_account", views.manage_account, name="manage_account"),
-]
 from UWEflix import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,6 +16,10 @@ urlpatterns = [
     path("update_club",views.update_club,name="update_club"),
     path("delete_club",views.delete_club,name="delete_club"),
     path("view_club",views.Club_list_view,name="view_club"),
+    path("shows", views.get_shows, name="get_shows"),
+    path("purchase_ticket", views.purchase_ticket, name="purchase_ticket"),
+    path("book_show", views.book_show, name="book_show"),
+    path("manage_account", views.manage_account, name="manage_account"),
 
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
