2abed3175ac930371d6584497b83827c6469f3aa
tengkuadrianna <tengkuadrianna01@gmail.com>
2023-03-27 01:22:24 +0100

    update seat and payment page

--- UWEflix/models/booking.py
+++ UWEflix/models/booking.py
@@ -6,16 +6,26 @@ class Booking(models.Model):
     # Fields for the booking model
     booking_id = models.AutoField(primary_key=True)
     show = models.ForeignKey(Show, on_delete=models.CASCADE)
-    ticket_type = models.CharField(max_length=7)
-    quantity = models.IntegerField()
+    
+    # ticket_adult = models.CharField(max_length=7)
+    # ticket_children = models.CharField(max_length=7)
+    # ticket_student = models.CharField(max_length=7)
+
+    quantity_adult = models.IntegerField()
+    quantity_children = models.IntegerField()
+    quantity_student = models.IntegerField()
+
     card_number = models.IntegerField(null=True)
     card_holder = models.CharField(max_length=30,null=True)
     card_expire_year  = models.IntegerField(null=True)
     card_expire_month  = models.IntegerField(null=True)
     card_cvc  = models.IntegerField(null=True)
 
+    email = models.EmailField(null=True)
+
     class Meta:
         pass
     
     def __str__(self):
-        return f'Booking {self.booking_id} for {self.show} ({self.quantity} {self.ticket_type} tickets)'
+        # return f'Booking {self.booking_id} for {self.show} ({self.quantity} {self.ticket_type} tickets)'
+        return f'Booking {self.booking_id} for {self.show}'
--- UWEflix/static/js/ticket.js
+++ UWEflix/static/js/ticket.js
@@ -1,50 +1,135 @@
-const number = document.getElementById("num")
-const price = document.getElementById("price")
-const category = document.getElementById("cat")
+const toastElement = document.querySelector('.toast-container');
+
+try {
+
+
+    const student_num = document.getElementById("student_num")
+    const children_num = document.getElementById("children_num")
+    const adult_num = document.getElementById("adult_num")
+    const price = document.getElementById("price")
+
+    const adult_minus = document.getElementById("adult_minus")
+    const adult_plus = document.getElementById("adult_plus")
+
+    const children_minus = document.getElementById("children_minus")
+    const children_plus = document.getElementById("children_plus")
+
+    const student_minus = document.getElementById("student_minus")
+    const student_plus = document.getElementById("student_plus")
+    const count = document.getElementById("count").dataset.capacity
 
 
 
-let count = 1;
-let adult = 10;
-let children = 8;
-let student = 7; 
-let option = null;
 
-console.log(number)
+    let adultCount = 0;
+    let studentCount = 0;
+    let childrenCount = 0;
 
-category.addEventListener("change", function (e) {
-    option = e.target.value
-    if (option === "Adult") return price.innerHTML = adult * count
-    if (option === "Children") return price.innerHTML = children * count
-    if (option === "Student") return price.innerHTML = student * count
-})
 
-// console.log(number.dataset.capacity)
+    let adultTotal = 0;
+    let studentTotal = 0;
+    let childrenTotal = 0;
 
-function increment() {
-    if (count < parseInt(number.dataset.capacity)  ){
-        count++;
+    let total = 0;
+
+    let adult = 10;
+    let children = 8;
+    let student = 7;
+
+
+
+    adult_minus.addEventListener('click', function (e) {
+        if (adultCount > 0) {
+            adultCount--;
+        }
+        adultTotal = adult * adultCount
+    })
+    student_minus.addEventListener('click', function (e) {
+        if (studentCount > 0) {
+            studentCount--;
+        }
+        studentTotal = student * studentCount
+    })
+    children_minus.addEventListener('click', function (e) {
+        if (childrenCount > 0) {
+            childrenCount--;
+        }
+        childrenTotal = children * childrenCount
+    })
+
+    adult_plus.addEventListener('click', function (e) {
+        if (adultCount + studentCount + childrenCount < count) {
+            adultCount++;
+        }
+        adultTotal = adult * adultCount
+    })
+    student_plus.addEventListener('click', function (e) {
+        if (adultCount + studentCount + childrenCount < count) {
+            studentCount++;
+        }
+        studentTotal = student * studentCount
+    })
+    children_plus.addEventListener('click', function (e) {
+        if (adultCount + studentCount + childrenCount < count) {
+            childrenCount++;
+        }
+        childrenTotal = children * childrenCount
+    })
+
+
+    function increment() {
+        adult_num.value = adultCount;
+        student_num.value = studentCount;
+        children_num.value = childrenCount;
+        total = adultTotal + studentTotal + childrenTotal
+        price.innerHTML = total
+        if (adultCount + studentCount + childrenCount === parseInt(count)) {
+            toastElement.classList.add('show');
+            setTimeout(() => {
+                toastElement.classList.remove('show');
+            }, 3000);
+        }
     }
-    
-    number.innerHTML = count;
-    // console.log(parseInt(number.dataset.capacity))
-    // console.log(count <= parseInt(number.dataset.capacity) )
 
-    if (option === "Adult") return price.innerHTML = adult * count
-    if (option === "Children") return price.innerHTML = children * count
-    if (option === "Student") return price.innerHTML = student * count
+    function decrement() {
+        adult_num.value = adultCount;
+        student_num.value = studentCount;
+        children_num.value = childrenCount;
+        total = adultTotal + studentTotal + childrenTotal
+        price.innerHTML = total
+    }
+} catch (e) {
+    console.log(e)
 }
 
-function decrement() {
-    if (count > 1) {
-        count--;
-        number.innerHTML = count;
-        if (option === "Adult") return price.innerHTML = adult * count
-        if (option === "Children") return price.innerHTML = children * count
-        if (option === "Student") return price.innerHTML = student * count
+
+try {
+    const houseFull = document.getElementById("house");
+
+    if (houseFull.dataset.dis === "True") {
+        houseFull.disabled = true
+
+        setInterval(() => {
+            toastElement.classList.add('show');
+        }, 1000);
     }
+
+
+} catch (error) {
+    console.log(error)
 }
 
-function backButton (){
-    window.history.back()
+try {
+
+    const btn = document.getElementsByClassName("dis")
+
+    for (let index = 0; index < btn.length; index++) {
+        console.log(typeof btn[index].dataset.dis, btn[index].dataset.dis, typeof true)
+        if (btn[index].dataset.dis === "True") {
+            btn[index].disabled = true
+        }
+    }
+
+} catch (error) {
+    console.log(error)
 }
